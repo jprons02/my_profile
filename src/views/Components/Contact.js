21acbb41a84e06e3cb4fa5c049595281f1a18ead
Joseph Ronselli <joseph.ronselli@yahoo.com>
2023-08-23 13:59:36 -0400

    updated examples

--- src/views/Components/Contact.js
+++ src/views/Components/Contact.js
@@ -1,45 +1,45 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { keys } from "keys.js";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { keys } from 'assets/keys.js';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
 // core components
-import Button from "components/CustomButtons/Button.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from 'components/CustomButtons/Button.js';
+import CustomInput from 'components/CustomInput/CustomInput.js';
 // styles
-import styles from "assets/jss/material-kit-react/views/componentsSections/loginStyle.js";
+import styles from 'assets/jss/material-kit-react/views/componentsSections/loginStyle.js';
 
 const useStyles = makeStyles(styles);
 
 const Contact = (props) => {
   const classes = useStyles();
 
-  const [nameValue, setNameValue] = useState("");
-  const [emailValue, setEmailValue] = useState("");
-  const [messageValue, setMessageValue] = useState("");
+  const [nameValue, setNameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [messageValue, setMessageValue] = useState('');
   const [loadingState, setLoadingState] = useState(false);
 
   const handleInputChange = (e) => {
-    if (e.target.id === "name") {
+    if (e.target.id === 'name') {
       setNameValue(e.target.value);
     }
-    if (e.target.id === "email") {
+    if (e.target.id === 'email') {
       setEmailValue(e.target.value);
     }
-    if (e.target.id === "message") {
+    if (e.target.id === 'message') {
       setMessageValue(e.target.value);
     }
   };
 
-  console.log("name ", nameValue);
+  console.log('name ', nameValue);
 
   const resetInputs = () => {
-    setNameValue("");
-    setEmailValue("");
-    setMessageValue("");
+    setNameValue('');
+    setEmailValue('');
+    setMessageValue('');
   };
 
   const handleSubmit = async (e) => {
@@ -50,28 +50,28 @@ const Contact = (props) => {
 
     const callback = (value) => {
       setLoadingState(false);
-      if (value === "error") {
-        alert("Error sending email, please try again later.");
+      if (value === 'error') {
+        alert('Error sending email, please try again later.');
       }
       resetInputs();
     };
 
     try {
       const response = await axios({
-        method: "POST",
-        url: "https://llnaae68vk.execute-api.us-east-1.amazonaws.com/prod",
+        method: 'POST',
+        url: 'https://llnaae68vk.execute-api.us-east-1.amazonaws.com/prod',
         data: { name: nameValue, email: emailValue, message: messageValue },
         headers: {
-          "Content-Type": "application/json",
-          "x-api-key": keys.portfolioSiteAPI,
+          'Content-Type': 'application/json',
+          'x-api-key': keys.portfolioSiteAPI,
         },
       });
       if (response.data) {
         callback();
       }
     } catch (error) {
-      console.log("error: ", error);
-      callback("error");
+      console.log('error: ', error);
+      callback('error');
     }
   };
 
@@ -89,7 +89,7 @@ const Contact = (props) => {
               }}
               inputProps={{
                 onChange: handleInputChange,
-                type: "text",
+                type: 'text',
               }}
             />
           </GridItem>
@@ -104,7 +104,7 @@ const Contact = (props) => {
               }}
               inputProps={{
                 onChange: handleInputChange,
-                type: "email",
+                type: 'email',
               }}
             />
           </GridItem>
@@ -124,7 +124,7 @@ const Contact = (props) => {
               }}
             />
           </GridItem>
-          <div style={{ position: "relative" }}>
+          <div style={{ position: 'relative' }}>
             <Button
               simple
               type="submit"
@@ -137,18 +137,18 @@ const Contact = (props) => {
             {loadingState ? (
               <CircularProgress
                 style={{
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  marginTop: "-12px",
-                  marginLeft: "-12px",
-                  color: "#9c27b0",
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  marginTop: '-12px',
+                  marginLeft: '-12px',
+                  color: '#9c27b0',
                 }}
                 size={24}
                 color="primary"
               />
             ) : (
-              ""
+              ''
             )}
           </div>
         </GridContainer>
